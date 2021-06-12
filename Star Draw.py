166d3239663193a2e019ce3a0826bd7f7c4694a1
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-12 23:40:17 +0530

    Update Star Draw.py

--- Star Draw.py
+++ Star Draw.py
@@ -6,8 +6,8 @@ screen =turtle.Screen()
 screen.bgcolor('black')
 t.speed(30)
 
-for i in range (150):
-    t.color(col[i%6])
+for i in range (350):
+    t.color(col[i%5])
     t.forward(1*4)
     t.left(150)
     t.width(2)
