96c5890c91f1f5b3b8cac897298c0345e9e9943f
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-12 23:26:39 +0530

    /

---
+++ turtle.py
@@ -0,0 +1,13 @@
+import turtle
+col = ('yellow','red', 'green','blue','white')
+
+t = turtle.Turtle()
+screen =turtle.Screen()
+screen.bgcolor('black')
+t.speed(30)
+
+for i in range (150):
+    t.color(col[i%6])
+    t.forward(1*4)
+    t.left(150)
+    t.width(2)
