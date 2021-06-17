33b5f6805d8eba7170c6488fb59aac29ce462c26
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-17 23:40:19 +0530

    Update Flower mytry .py

---
+++ Flower mytry .py
@@ -0,0 +1,18 @@
+#code form riju_ghosh___ instagram profile
+
+import turtle
+turtle.setworldcoordinates(-2000,-2000,2000,2000)
+
+def draw_football(x,y,tilt,radius):
+  turtle.pencolor('green')
+  turtle.up()
+  turtle.goto(x,y)
+  turtle.down()
+  turtle.seth(tilt-45)
+  turtle.circle(radius,90)
+  turtle.left(90)
+  turtle.circle(radius,90)
+
+for tilt in range(0,360,30):
+    
+    draw_football(0,0,tilt,1000) 
