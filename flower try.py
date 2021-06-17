f6e62b48f9ea41cbc6532fc0adb65b342c87f121
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-17 23:36:50 +0530

    Update flower try.py

---
+++ flower try.py
@@ -0,0 +1,16 @@
+import turtle
+turtle.setworldcoordinates(-2000,-2000,2000,2000)
+
+def draw_football(x,y,tilt,radius):
+  turtle.pencolor('orange')
+  turtle.up()
+  turtle.goto(x,y)
+  turtle.down()
+  turtle.seth(tilt-45)
+  turtle.circle(radius,90)
+  turtle.left(90)
+  turtle.circle(radius,90)
+
+for tilt in range(0,360,30):
+    
+    draw_football(0,0,tilt,1000) 
