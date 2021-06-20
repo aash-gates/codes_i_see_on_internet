2790c3fd0f92279152496b3bc8a91044d2d01ccc
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-20 23:54:48 +0530

    Update the big b .py

--- the big b .py
+++ the big b .py
@@ -1,7 +1,5 @@
 from turtle import Turtle, mainloop
-from time import perf_counter as clock
-
-# wrapper for any additional drawing routines
+froor any additional drawing routines
 # that need to know about each other
 elc
   
