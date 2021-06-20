40059f82138e580264e58e9a628f7df57558b82c
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-20 23:54:23 +0530

    Update the big b .py

--- the big b .py
+++ the big b .py
@@ -3,10 +3,7 @@ from time import perf_counter as clock
 
 # wrapper for any additional drawing routines
 # that need to know about each other
-class Designer(Turtle):
-
-    def design(self, homePos, scale):
-        selckward(64.65 * scale)
+elckward(64.65 * scale)
           
  
   
