9ec36c730c771bdc9ab6bc2b8104efba9bb0ee72
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-20 23:55:36 +0530

    Update the big b .py

---
+++ the big b .py
@@ -0,0 +1,21 @@
+from turtle import Turtle, mainloop
+from time import perf_counter as clock
+
+# wrapper for any additional drawing routines
+# that need to know about each other
+class Designer(Turtle):
+
+    def design(self, homePos, scale):
+        self.up()
+        for i in range(5):
+            self.forward(64.65 * scale)
+          f.backward(64.65 * scale)
+            ft(36)
+        for
+   
+ 
+ 
+  
+
+
+
