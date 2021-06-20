ffb3d7686e12d473af7ee2b11acd680535404340
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-21 00:06:00 +0530

    Update simple password generator.py

--- simple password generator.py
+++ simple password generator.py
@@ -8,14 +8,4 @@ password =  "".join(choice(characters) for x in range(randint(8,16)))
 
 print (password)
 
-#End of the program#a simple password generator
-
-import string
-from random import *
-
-characters = string.ascii_letters + string.punctuation  + string.digits
-password =  "".join(choice(characters) for x in range(randint(8,16)))
-
-print (password)
-
 #End of the program
