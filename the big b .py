4335f391b8b18d50d0cf68f06a53d7a1b668bd5a
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-20 23:56:01 +0530

    Update the big b .py

--- the big b .py
+++ the big b .py
@@ -4,10 +4,43 @@ from time import perf_counter as clock
 # wrapper for any additional drawing routines
 # that need to know about each other
 class Designer(Turtle):
-    
+
     def design(self, homePos, scale):
-        selckward(64.65 * scale)
-          
+        self.up()
+        for i in range(5):
+            self.forward(64.65 * scale)
+            self.down()
+            self.wheel(self.position(), scale)
+            self.up()
+            self.backward(64.65 * scale)
+            self.right(72)
+        self.up()
+        self.goto(homePos)
+        self.right(36)
+        self.forward(24.5 * scale)
+        self.right(198)
+        self.down()
+        self.centerpiece(46 * scale, 143.4, scale)
+        self.getscreen().tracer(True)
+
+    def wheel(self, initpos, scale):
+        self.right(54)
+        for i in range(4):
+            self.pentpiece(initpos, scale)
+        self.down()
+        self.left(36)
+        for i in range(5):
+            self.tripiece(initpos, scale)
+        self.left(36)
+        for i in range(5):
+           
+      
+
+
+ 
+    
+   
+ 
  
   
 
