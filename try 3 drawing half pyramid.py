a057f01da7bec3ccbf156defdc5f24be792e1b1e
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-14 23:46:59 +0530

    Delete try 3 drawing half pyramid.py

--- try 3 drawing half pyramid.py
+++
@@ -1,9 +0,0 @@
-rows =int(input("enter numbers of rows: "))
-for i in range(rows):
-    for j in range(i+1):
-        import time
-        time.sleep(0.55)
-        print(j+1, end=" ")
-    print("\n")
-
-
