7ed116b01a3eaa1fd8eb57ab6ff61e56629c82b9
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-24 23:19:00 +0530

    Create mytry with turtle module1 .py

---
+++ mytry with turtle module1 .py
@@ -0,0 +1,24 @@
+#trying out the turtle module
+
+import turtle
+
+
+turtle.write("Aash Gates Star", font=("Blackadder ITC",))
+
+import time
+time.sleep(2.00)
+
+
+start = turtle.Screen()
+
+star = turtle.Turtle()
+
+turtle.speed ('slowest')
+
+
+
+for i in range(5):
+    star.forward(144)
+    
+    star.right(144)
+    
