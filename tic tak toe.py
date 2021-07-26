616986fdbbbbde1a849942ee0209d45a90d2d050
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-26 22:06:15 +0530

    Update tic tak toe.py

--- tic tak toe.py
+++ tic tak toe.py
@@ -71,3 +71,4 @@ def checkForWin():
 
     elif(button1['text']=='O' and button2['text']=='O' and button3['text']=='O' or
             button4['text']=='O' and button5['text']=='O' and button6['text']=='O' or
+            button7['text']=='O' and button8['text']=='O' and button9['text']=='O' or
