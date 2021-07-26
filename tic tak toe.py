df15dc3b942b0f14fc3d2827ebf69d8e754aae5a
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-26 22:06:24 +0530

    Update tic tak toe.py

--- tic tak toe.py
+++ tic tak toe.py
@@ -74,3 +74,4 @@ def checkForWin():
             button7['text']=='O' and button8['text']=='O' and button9['text']=='O' or
             button1['text']=='O' and button4['text']=='O' and button7['text']=='O' or
             button2['text']=='O' and button5['text']=='O' and button8['text']=='O' or
+            button3['text']=='O' and button6['text']=='O' and button9['text']=='O' or
