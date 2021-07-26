e41e13dbcc256a1c3f7d46946664f31e66a3b77b
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-26 21:49:36 +0530

    Update pikachu.py

--- pikachu.py
+++ pikachu.py
@@ -19,3 +19,13 @@ class Pikachu:
     def noTrace_goto(self, x, y):
         self.t.penup()
         self.t.goto(x, y)
+        self.t.pendown()
+
+    def leftEye(self, x, y):
+        self.noTrace_goto(x, y)
+        t = self.t
+        t.seth(0)
+        t.fillcolor('#333333')
+        t.begin_fill()
+        t.circle(22)
+        t.end_fill()
