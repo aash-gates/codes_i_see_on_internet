9bf56732e44a7589751cf99b25bb6214c99d09b6
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-26 21:49:46 +0530

    Update pikachu.py

--- pikachu.py
+++ pikachu.py
@@ -29,3 +29,13 @@ class Pikachu:
         t.begin_fill()
         t.circle(22)
         t.end_fill()
+
+        self.noTrace_goto(x, y + 10)
+        t.fillcolor('#000000')
+        t.begin_fill()
+        t.circle(10)
+        t.end_fill()
+
+        self.noTrace_goto(x + 6, y + 22)
+        t.fillcolor('#ffffff')
+        t.begin_fill()
