2cbddf209167efa09a575548ad872f5da851323b
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-08-03 19:59:39 +0530

    Update Gowdara help.py

--- Gowdara help.py
+++ Gowdara help.py
@@ -4,3 +4,4 @@ radius=(1/2*diameter)
 # Create a variable called 'radius' equal to half the diameter
 ____
 area=(pi*radius^2)
+# Create a variable called 'area', using the formula for the area of a circle: pi times the radius squared
