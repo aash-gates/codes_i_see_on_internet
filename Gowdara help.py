83e48e1ad39593fb456971ffb4914c1289e3534b
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-08-04 21:41:34 +0530

    Create Gowdara help.py

---
+++ Gowdara help.py
@@ -0,0 +1,12 @@
+pi = 3.14159 # approximate
+diameter = 3
+radius=(1/2*diameter)
+# Create a variable called 'radius' equal to half the diameter
+____
+area=(pi*radius^2)
+# Create a variable called 'area', using the formula for the area of a circle: pi times the radius squared
+____
+
+# Check your answer
+q1.check()
+
